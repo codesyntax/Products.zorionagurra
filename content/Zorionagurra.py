c867204dbbadbf714316180f500d1dd759db9b1b
Mikel Larreategi <mlarreategi@codesyntax.com>
2007-07-24 15:15:39 +0000

    goienako zorion agurraren antzeko zerbait egin

--- content/Zorionagurra.py
+++ content/Zorionagurra.py
@@ -12,6 +12,8 @@ schema = Schema((
 
     StringField(
         name='name',
+        required=1,
+        searchable=0
         widget=StringWidget(
             label='Name',
             label_msgid='zorionagurra_label_name',
@@ -22,6 +24,8 @@ schema = Schema((
 
     StringField(
         name='surname',
+        required=1,
+        searchable=0,
         widget=StringWidget(
             label='Surname',
             label_msgid='zorionagurra_label_surname',
@@ -30,31 +34,48 @@ schema = Schema((
         )
     ),
 
-    StringField(
+    ComputedField(
         name='title',
+        expression='context.computeFullname()',
+        searchable=1,
+        accessor='Title',
         widget=StringWidget(
             label='Title',
             label_msgid='zorionagurra_label_title',
             description_msgid='zorionagurra_help_title',
             i18n_domain='zorionagurra',
+            visible={'view': 'visible',
+                     'edit': 'invisible',
+                     'roster_list': 'visible',
+                     },
         )
     ),
 
     TextField(
         name='text',
-        allowable_content_types=('text/plain', 'text/structured', 'text/html', 'application/msword',),
-        widget=RichWidget(
+        required=1,
+        searchable=1,
+        allowable_content_types=('text/plain',),
+        widget=TextAreaWidget(
+            rows=5,
+            cols=40,
             label='Text',
             label_msgid='zorionagurra_label_text',
             description_msgid='zorionagurra_help_text',
             i18n_domain='zorionagurra',
         ),
-        default_output_type='text/html',
-        searchable=1
+        default_output_type='text/plain',
+        default_content_type='text/plain',
     ),
 
     ImageField(
         name='photo',
+        required=1,
+        sizes={
+              'thumb':(50,50),
+              'mini':(150,150),
+              'preview':(400,400),
+              },
         widget=ImageWidget(
             label='Photo',
             label_msgid='zorionagurra_label_photo',
@@ -69,6 +90,25 @@ schema = Schema((
 
 Zorionagurra_schema = BaseSchema.copy() + \
     schema.copy()
+
+Zorionagurra_schema['effectiveDate'].schemata = 'default'
+Zorionagurra_schema['effectiveDate'].default = DateTime.DateTime()
+Zorionagurra_schema['effectiveDate'].widget.show_hm = False
+Zorionagurra_schema['effectiveDate'].widget.label='Data'
+Zorionagurra_schema['effectiveDate'].widget.label_msgid='zorionagurara_label_date'
+Zorionagurra_schema['effectiveDate'].widget.description='Select the day of this element'
+Zorionagurra_schema['effectiveDate'].widget.description_msgid='zorionagurra_help_date'
+Zorionagurra_schema['effectiveDate'].widget.i18n_domain='zorionagurra'
+
+Zorionagurra_schema['description'].required = 0
+Zorionagurra_schema['description'].widget.visible = {'edit':'hidden',
+                                                     'view':'hidden',
+                                                     }
+Zorionagurra_schema['allowDiscussion'].widget.visible = {'edit':'hidden',
+                                                         'view':'hidden',
+                                                         }
+Zorionagurra_schema.moveField('effectiveDate', after='surname')
+
 
 class Zorionagurra(BaseContent):
     """
@@ -96,6 +136,18 @@ class Zorionagurra(BaseContent):
     schema = Zorionagurra_schema
 
     # Methods
+    def at_post_create_script(self):
+        """ Post creation hook """
+        self._renameAfterCreation()
+
+    def computeFullname(self):
+        name = self.getField('name').getAccessor(self)()
+        surname = self.getField('surname').getAccessor(self)()
+        if name and surname:
+            space = ' '
+        else:
+            space = ''
+        return "%s%s%s" % (name, space, surname)
 
 
 registerType(Zorionagurra, PROJECTNAME)
