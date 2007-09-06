26e664b95877a7def4ea7ab21fd4a9dd3882b168
Mikel Larreategi <mlarreategi@codesyntax.com>
2007-09-06 08:36:34 +0000

    Itzulpenak

---
+++ i18n/createi18n.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+# recreate the pot file from skin page templates
+i18ndude rebuild-pot --pot zorionagurra.pot --create zorionagurra ../skins ../browser 
+# append the pot file and the firstly generated one
+cat manual.pot >> zorionagurra.pot
+cat generated.pot >> zorionagurra.pot
+# append new msgids to language specific PO files
+i18ndude sync --pot zorionagurra.pot zorionagurra-??.po
