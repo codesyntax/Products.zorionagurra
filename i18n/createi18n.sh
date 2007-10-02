b7bd484bbb56e0ab66e412635dc77b3279ae5f9d
Mikel Larreategi <mlarreategi@codesyntax.com>
2007-10-02 10:36:51 +0000

    Itzulpneka

--- i18n/createi18n.sh
+++ i18n/createi18n.sh
@@ -7,3 +7,4 @@ cat manual.pot >> zorionagurra.pot
 cat generated.pot >> zorionagurra.pot
 # append new msgids to language specific PO files
 i18ndude sync --pot zorionagurra.pot zorionagurra-??.po
+i18ndude sync --pot zorionagurra-plone.pot zorionagurra-plone-??.po
